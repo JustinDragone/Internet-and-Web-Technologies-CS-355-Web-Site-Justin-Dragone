c0caae037a9439fe5a6642d5ca780ab85cc0ddf9
Justin Dragone <jdragone1@gmail.com>
2013-04-18 20:28:28 -0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1>About Us</h1>
+<p>We specialize in selling video games from Playstation 3, Xbox 360, and Wii</p>
+
+<h1>Return Policy</h1>
+<p>You have 30 days to return items if not open and 14 if there is a problem and need another one.</p>
+
+</body>
+</html>
